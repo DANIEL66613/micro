4cfbcdd478d0dd04c3cd53713d378070925dffab
DANIEL66613 <104590003+DANIEL66613@users.noreply.github.com>
2024-10-18 19:56:52 -0300

    atualizaçao de acordo com a do professor

--- flaskr/models/turma_model.py
+++ flaskr/models/turma_model.py
@@ -1,11 +1,48 @@
+from config import db
+
 class Turma(db.Model):
-    __tablename__ = 'turmas'
-    
     id = db.Column(db.Integer, primary_key=True)
-    descricao = db.Column(db.String(200), nullable=False)
-    professor_id = db.Column(db.Integer, db.ForeignKey('professores.id'), nullable=False)
-    ativo = db.Column(db.Boolean, default=True)
-    alunos = db.relationship('Aluno', backref='turma', lazy=True)
-    
-    def __repr__(self):
-        return f"<Turma {self.descricao}>"
+    descricao = db.Column(db.String(100))
+    professor = db.Column(db.String(100))
+    ativo = db.Column(db.Bool)
+
+    def __init__(self, nome, descricao, professor, ativo):
+        self.nome = nome
+        self.descricao = descricao
+        self.professor = professor
+        self.ativo = ativo
+
+    def to_dict(self):
+        return {'id': self.id, 'nome': self.nome, 'descricao': self.descricao, 'professor':self.professor, 'ativo': self.ativo}
+
+class TurmaNaoEncontrado(Exception):
+    pass
+
+def turma_por_id(id_turma):
+    turma = Turma.query.get(id_turma)
+    if not turma:
+        raise TurmaNaoEncontrado
+    return turma.to_dict()
+
+def listar_turmas():
+    turma = Turma.query.all()
+    return [turma.to_dict() for turma in turmas]
+
+def adicionar_turma(turma_data):
+    nova_turma = Turma(nome=turma_data['nome'])
+    db.session.add(nova_turma)
+    db.session.commit()
+
+def atualizar_turma(id_turma, novos_dados):
+    turma = Turma.query.get(id_turma)
+    if not turma:
+        raise TurmaNaoEncontrado
+    turma.nome = novos_dados['nome']
+    db.session.commit()
+
+def excluir_turma(id_turma):
+    turma = Turma.query.get(id_turma)
+    if not turma:
+        raise TurmaNaoEncontrado
+    db.session.delete(turma)
+    db.session.commit()
