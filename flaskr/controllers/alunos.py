287c26e4be077dfede8930cc20cffd3d9430e818
DANIEL66613 <104590003+DANIEL66613@users.noreply.github.com>
2024-10-18 14:18:47 -0300

    18/10 com tratamento de rros

--- flaskr/controllers/alunos.py
+++ flaskr/controllers/alunos.py
@@ -3,7 +3,7 @@ from flask import Blueprint, request, jsonify
 # Criação do blueprint corretamente com letra maiúscula
 alunos_blueprint = Blueprint('alunos', __name__)
 
-# Lista para armazenar alunos
+# Lista temporária para armazenar alunos
 alunos = []
 
 @alunos_blueprint.route('/alunos', methods=['GET'])
@@ -13,19 +13,22 @@ def get_alunos():
 @alunos_blueprint.route('/alunos', methods=['POST'])
 def create_aluno():
     data = request.json
+    # Validação básica de dados (pode ser mais elaborada)
+    if not data or 'nome' not in data or 'idade' not in data:
+        return jsonify({'erro': 'Dados inválidos ou incompletos'}), 400
+
     novo_aluno = {
         'id': len(alunos) + 1,
         'nome': data['nome'],
         'idade': data['idade'],
-        'turma': data['turma'],
-        'data_nascimento': data['data_nascimento'],
-        'nota_primeiro_semestre': data['nota_primeiro_semestre'],
-        'nota_segundo_semestre': data['nota_segundo_semestre'],
-        'media_final': data['media_final']
+        'turma': data.get('turma', 'Sem turma'),  # Valor padrão se não informado
+        'data_nascimento': data.get('data_nascimento', 'Não informado'),
+        'nota_primeiro_semestre': data.get('nota_primeiro_semestre', 0),
+        'nota_segundo_semestre': data.get('nota_segundo_semestre', 0),
+        'media_final': data.get('media_final', 0)
     }
     alunos.append(novo_aluno)
     return jsonify(novo_aluno), 201
-
 
 @alunos_blueprint.route('/alunos/<int:aluno_id>', methods=['PUT'])
 def update_aluno(aluno_id):
@@ -41,7 +44,6 @@ def update_aluno(aluno_id):
             aluno['media_final'] = data.get('media_final', aluno['media_final'])
             return jsonify(aluno)
     return jsonify({'mensagem': 'Aluno não encontrado'}), 404
-
 
 @alunos_blueprint.route('/alunos/<int:aluno_id>', methods=['DELETE'])
 def delete_aluno(aluno_id):
