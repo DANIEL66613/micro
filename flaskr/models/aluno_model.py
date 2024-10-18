8a21c86dca97f25b73958699aa582e16ae3a8125
DANIEL66613 <104590003+DANIEL66613@users.noreply.github.com>
2024-10-18 19:21:57 -0300

    atualização de acordo com o a professor

--- flaskr/models/aluno_model.py
+++ flaskr/models/aluno_model.py
@@ -1,18 +1,43 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from config import db
 
 class Aluno(db.Model):
-    __tablename__ = 'alunos'
-    
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100), nullable=False)
-    idade = db.Column(db.Integer, nullable=False)
-    turma_id = db.Column(db.Integer, db.ForeignKey('turmas.id'), nullable=False)
-    data_nascimento = db.Column(db.String(10), nullable=False)
-    nota_primeiro_semestre = db.Column(db.Float)
-    nota_segundo_semestre = db.Column(db.Float)
-    media_final = db.Column(db.Float)
-    
-    def __repr__(self):
-        return f"<Aluno {self.nome}>"
+    nome = db.Column(db.String(100))
+
+    def __init__(self, nome):
+        self.nome = nome
+
+    def to_dict(self):
+        return {'id': self.id, 'nome': self.nome}
+
+class AlunoNaoEncontrado(Exception):
+    pass
+
+def aluno_por_id(id_aluno):
+    aluno = Aluno.query.get(id_aluno)
+    if not aluno:
+        raise AlunoNaoEncontrado
+    return aluno.to_dict()
+
+def listar_alunos():
+    alunos = Aluno.query.all()
+    return [aluno.to_dict() for aluno in alunos]
+
+def adicionar_aluno(aluno_data):
+    novo_aluno = Aluno(nome=aluno_data['nome'])
+    db.session.add(novo_aluno)
+    db.session.commit()
+
+def atualizar_aluno(id_aluno, novos_dados):
+    aluno = Aluno.query.get(id_aluno)
+    if not aluno:
+        raise AlunoNaoEncontrado
+    aluno.nome = novos_dados['nome']
+    db.session.commit()
+
+def excluir_aluno(id_aluno):
+    aluno = Aluno.query.get(id_aluno)
+    if not aluno:
+        raise AlunoNaoEncontrado
+    db.session.delete(aluno)
+    db.session.commit()
