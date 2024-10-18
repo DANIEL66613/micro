30edfea3722fa5cecc9856f8e280aec0e97ba355
DANIEL66613 <104590003+DANIEL66613@users.noreply.github.com>
2024-10-18 19:39:43 -0300

    atualizaççao conforme a do professor

--- flaskr/models/professores_model.py
+++ flaskr/models/professores_model.py
@@ -1,11 +1,50 @@
-class Professor(db.Model):
-    __tablename__ = 'professores'
-    
+from config import db
+
+class Professores(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100), nullable=False)
-    idade = db.Column(db.Integer, nullable=False)
-    materia = db.Column(db.String(100), nullable=False)
-    observacoes = db.Column(db.Text)
+    nome = db.Column(db.String(100))
+    idade = db.Column(db.Integer)
+    materia = db.Column(db.String(100))
+    observacoes = db.Column(db.String(100))
     
-    def __repr__(self):
-        return f"<Professor {self.nome}>"
+
+    def __init__(self, nome, idade, materia, observacoes):
+        self.nome = nome
+        self.idade = idade
+        self.materia = materia
+        self.observacoes = observacoes
+
+    def to_dict(self):
+        return {'id': self.id, 'nome': self.nome, 'idade': self.idade, 'materia': self.materia, 'observacoes': self.observacoes}
+
+class ProfessorNaoEncontrado(Exception):
+    pass
+
+def professor_por_id(id_professor):
+    professor = Professor.query.get(id_professor)
+    if not professor:
+        raise ProfessorNaoEncontrado
+    return professor.to_dict()
+
+def listar_professor():
+    professor = Professores.query.all()
+    return [professor.to_dict() for professor in professores]
+
+def adicionar_professor(profesor_data):
+    novo_professor = Professores(nome=professor_data['nome'])
+    db.session.add(novo_professor)
+    db.session.commit()
+
+def atualizar_professor(id_professor, novos_dados):
+    professor = Professor.query.get(id_professor)
+    if not professor:
+        raise PROFESSORNaoEncontrado
+    professor.nome = novos_dados['nome']
+    db.session.commit()
+
+def excluir_professor(id_professor):
+    professor = Professor.query.get(id_professor)
+    if not professor:
+        raise ProfessorNaoEncontrado
+    db.session.delete(professor)
+    db.session.commit()
